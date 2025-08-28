f5e86fd503c12259ae3c2e7133eaac1ba63d1e78
Uncle Scientist <unclescientist@gmail.com>
2025-08-28 07:18:45 -0400

    The Entertainment Hub, Quest 2, Part 2

--- quest2/src/main.rs
+++ quest2/src/main.rs
@@ -1,15 +1,28 @@
+const SEQ: [char; 3] = ['R', 'G', 'B'];
+
 fn main() {
     let lines = aoclib::read_lines("input/everybody_codes_e2_q02_p1.txt");
     let mut balloons = BalloonQueue::new(&lines[0]);
     let mut _balloons = BalloonQueue::new("GRBGGGBBBRRRRRRRR");
-    let seq = ['R', 'G', 'B'];
     let mut bolts = 0;
     while !balloons.popped() {
-        let fluffbolt = seq[bolts % 3];
+        let fluffbolt = SEQ[bolts % 3];
         bolts += 1;
         balloons.shoot(fluffbolt);
     }
     println!("part 1 = {bolts}");
+
+    let lines = aoclib::read_lines("input/everybody_codes_e2_q02_p2.txt");
+    let mut balloons = BalloonQueue::new(&lines[0]);
+    let mut _balloons = BalloonQueue::new("BBRGGRRGBBRGGBRGBBRRBRRRBGGRRRBGBGG");
+    balloons.extend_circle(100);
+    let mut bolts = 0;
+    while !balloons.popped() {
+        let fluffbolt = SEQ[bolts % 3];
+        bolts += 1;
+        balloons.circle_shoot(fluffbolt);
+    }
+    println!("part 2 = {bolts}");
 }
 
 struct BalloonQueue {
@@ -25,6 +38,10 @@ impl BalloonQueue {
         }
     }
 
+    fn extend_circle(&mut self, times: usize) {
+        self.queue = self.queue.repeat(times);
+    }
+
     fn popped(&self) -> bool {
         self.index >= self.queue.len()
     }
@@ -34,5 +51,13 @@ impl BalloonQueue {
             self.index += 1;
         }
         self.index += 1;
+    }
+
+    fn circle_shoot(&mut self, fluffbolt: char) {
+        let len = self.queue.len();
+        if self.queue[0] == fluffbolt && len % 2 == 0 {
+            self.queue.remove(len / 2);
+        }
+        self.queue.remove(0);
     }
 }
